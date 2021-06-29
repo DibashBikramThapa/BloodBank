0285a078304f86092f3aedbde0af85a724e52770
DibashBikramThapa <dwasthapa172@gmail.com>
2021-06-29 21:30:31 +0545

    changes

--- bloodproject/settings.py
+++ bloodproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-zqy*_hk9b%)towm2g)t67&x9mb#y0mvq6_felq=-!(i-hxfn7#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blooddonorsystem.herokuapp.com','localhost', '127.0.0.1' ]
 
 
 # Application definition
