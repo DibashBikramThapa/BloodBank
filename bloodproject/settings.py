3fb36f5cb496f17bece6dd83ee0884304b773f8f
DibashBikramThapa <dwasthapa172@gmail.com>
2021-07-03 19:24:41 +0545

    mysql database

--- bloodproject/settings.py
+++ bloodproject/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'bloodproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_bloodproject',
+        'HOST':'127.0.0.1',
+        'USER': 'root',
+        'PASSWORD':'',
+        'PORT':'3306',
+        'ENGINE':'django.db.backends.mysql',
+
     }
 }
 
